84c6870c74b877ae88b1ccd31898b15bed93cabf
cesar <ca2longoria@gmail.com>
2016-08-26 02:02:57 -0500

    some update, surely

--- pyfu.py
+++ pyfu.py
@@ -173,7 +173,8 @@ def clone_layer_tree(img,layer,prefix='copy_',root=None):
 		# These can be executed later in succession on calling the returned
 		# insert_layers function.
 		
-		# WARNING: This still results in a backwards insertion!
+		# WARNING: This still results in a backwards insertion!  ... Or not?
+		#   Now, I am not seeing this.  Will have to look out for this one.
 		args.append((pdb.gimp_image_insert_layer,img,layer,
 			len(parents) and parents[-1]['newlayer'] or root,
 			ob['index']))
@@ -187,3 +188,210 @@ def clone_layer_tree(img,layer,prefix='copy_',root=None):
 			a[0](*a[1:])
 	
 	return bob[0],args,insert_layers
+
+
+import copy
+import types
+class observer(object):
+	"""
+	More of a flat table, right now, than a full-out object.
+	"""
+	class _Observer(object):
+		def __init__(self,classtype=None):
+			if hasattr(self,'_etable'):
+				return
+			# None covers all callbacks without a key.
+			self._etable = {None:{}}
+			self._ktable = {}
+			self._classtype = classtype
+		
+		def _add_events_1(self,*args):
+			for a in args:
+				self._etable[None][a] = []
+		def _add_events_k(self,*args):
+			for a in args:
+				self._ktable[a] = []
+		
+		def __parse_on(self,args):
+			k = None
+			call = None
+			if len(args) < 1:
+				raise Exception('argument(s) (k,)call is/are necessary')
+			# Set k and call.
+			if type(args[0]) is types.FunctionType:
+				call = args[0]
+				args = args[1:]
+			else:
+				k = args[0]
+				call = args[1]
+				args = args[2:]
+			if call is None:
+				raise Exception('Must provide callback')
+			return k,call,args
+		def on(self,op,*args,**keys):
+			try:
+				k,call,args = self.__parse_on(args)
+			except Exception as e:
+				print 'ERROR:',e
+				return False
+			# This will only fire for 'set' and 'del', assuming a key was
+			# appropriately provided.
+			if not self._etable.has_key(k):
+				self._etable[k] = copy.deepcopy(self._ktable)
+			# Add the callback.
+			r = self._etable[k][op]
+			r.append((call,args,keys))
+			return True
+		def off(self,op,all=False,*args,**keys):
+			try:
+				k,call,args = self.__parse_on(args)
+			except Exception as e:
+				print 'ERROR:',e
+				return False
+			if not self._etable.has_key(k):
+				return False
+			r = self._etable[k][op]
+			for i in range(len(r)):
+				c,ag,kw = r
+				if c == call:
+					del r[i]
+					if not all:
+						break
+			return True
+	
+	class _SetDel(_Observer):
+		def __init__(self,classtype):
+			observer._Observer.__init__(self,classtype)
+			self._add_events_k('set','del')
+		def __setitem__(self,k,v):
+			print 'setitem',k,v
+			old = self[k]
+			self._classtype.__setitem__(self,k,v)
+			if self._etable.has_key(k):
+				r = self._etable[k]['set']
+				for call,args,keys in r:
+					# new, old, key
+					call(v,old,k,*args,**keys)
+		def __delitem__(self,k):
+			print 'delitem',k
+			old = self[k]
+			self._classtype.__delitem__(self,k)
+			if self._etable.has_key(k):
+				r = self._etable[k]['del']
+				for call,args,keys in r:
+					call(old,k,*args,**keys)
+	
+	class _AddRemove(_Observer):
+		def __init__(self,classtype):
+			observer._Observer.__init__(self,classtype)
+			self._add_events_1('add','remove')
+		def _add_call(self,a,*args,**keys):
+			print '_add',a
+			r = self._etable[None]['add']
+			for call,args2,keys2 in r:
+				# This differs from key calls, as in _SetDel.  If an index, for
+				# example, is provided, then that prepends the args stored under
+				# (call,args,keys).
+				call(a,*(args+args2),**keys2)
+		def _remove_call(self,a,*args,**keys):
+			print '_add',a
+			r = self._etable[None]['remove']
+			for call,args2,keys2 in r:
+				# Looks like it ignores keys.
+				call(a,*(args+args2),**keys2)
+	
+	class _Reorder(_Observer):
+		def __init__(self,classtype):
+			if not hasattr(classtype,'__iter__'):
+				raise Exception('Must be based off an iterable class')
+			observer._Observer.__init__(self,classtype)
+			self._add_events_1('reorder')
+		def _reorder_call(self,*args,**keys):
+			print '_reorder_call',args,keys
+			r = self._etable[None]['reorder']
+			for call,args2,keys2 in r:
+				call(self,*(args+args2),**keys2)
+	
+	class List(_SetDel,_AddRemove,_Reorder,list):
+		def __init__(self,*args,**keys):
+			observer._SetDel.__init__(self,list)
+			observer._AddRemove.__init__(self,list)
+			observer._Reorder.__init__(self,list)
+			list.__init__(self,*args,**keys)
+			print 'init',args,keys
+		def __setslice__(self,a,b,r,*args,**keys):
+			print 'setslice',a,b,r
+			old_slice = self[a:b]
+			list.__setslice__(self,a,b,r,*args,**keys)
+			# Because the removal and insertion has already occurred, the self
+			# passed into the _add_calls will not represent the state of the
+			# list at time of insertion.
+			for i in range(a,b):
+				self._remove_call(old_slice[i-a],i)
+			for i in range(a,a+len(r)):
+				self._add_call(r[i-a],i,self)
+		def __delslice__(self,a,b,*args,**keys):
+			print 'delslice',a,b
+			old_slice = self[a:b]
+			list.__delslice__(self,*args,**keys)
+			for i in range(a,b):
+				if self._etable.has_key(i):
+					r = self._etable[i]['del']
+					for call,args2,keys2 in r:
+						call(old_slice[i-a],i,*args2,**keys2)
+				self._remove_call(old_slice[i-a],i)
+		def append(self,v,*args,**keys):
+			list.append(self,v)
+			self._add_call(v,len(self),self)
+		def extend(self,r,*args,**keys):
+			old_len = len(self)
+			list.extend(self,r,*args,**keys)
+			for i in range(old_len,len(self)):
+				self._add_call(self[i],old_len+i,self)
+		def insert(self,i,v,*args,**keys):
+			list.insert(self,i,v,*args,**keys)
+			self._add_call(v,i,self)
+		def remove(self,v,*args,**keys):
+			i = self.index(v)
+			list.remove(self,v,*args,**keys)
+			self._remove_call(v,i,self)
+		def reverse(self,*args,**keys):
+			list.reverse(self,*args,**keys)
+			self._reorder_call()
+		def sort(self,*args,**keys):
+			list.sort(self,*args,**keys)
+			self._reorder_call()
+	
+	
+	def __init__(self,d={}):
+		self._dict = dict(d)
+		self._etable = {}
+	
+	def __getitem__(self,k):
+		return self._dict[k]
+	
+	def __setitem__(self,k,v):
+		old = self._dict.has_key(k) and self._dict[k] or None
+		self._dict[k] = v
+		if self._etable.has_key(k):
+			for call,args,keywords in self._etable[k]['set']:
+				call(k,v,old,*args,**keywords)
+	
+	def __delitem__(self,k):
+		del self._dict[k]
+		if self._etable.has_key(k):
+			del self._etable[k]
+	
+	def on(self,k,op,call,*args,**keywords):
+		if not self._etable.has_key(k):
+			self._etable[k] = {'set':[],'del':[]}
+		self._etable[k][op].append((call,args,keywords))
+	
+	def off(self,k,op,call):
+		if not self._etable.has_key(k):
+			return
+		r = self._etable[k][op]
+		for i in range(len(r)):
+			if r[i] == call:
+				del r[i]
+
